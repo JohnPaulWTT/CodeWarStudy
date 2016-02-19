b2b402875100dd1b942cf2624684d4127db2a13f
JohnPaulWTT <614431853@qq.com>
2016-02-19 20:01:09 +0800

    新增两个题目

--- CSharpTest/CSharpTest/Kata.cs
+++ CSharpTest/CSharpTest/Kata.cs
@@ -39,5 +39,25 @@ namespace CSharpTest
             return (x * y).ToString().OrderBy(c => c).SequenceEqual((x.ToString() + y).OrderBy(c => c));
         }
         #endregion
+
+        #region 数组中字符串长度的极差
+        //给定两个数组字符串a1和a2，每个字符串是由字母从a到z。让X成为a1中任意一个字符串，y成为a2中任意一个字符串，求max(abs(length(x) − length(y)))
+        public static int Mxdiflg(string[] a1, string[] a2)
+        {
+            if (a1.Length <= 0 || a2.Length <= 0)
+                return -1;
+            var first = Math.Abs(a1.Max(x => x.Length) - a2.Min(x => x.Length));
+            var second = Math.Abs(a2.Max(x => x.Length) - a1.Min(x => x.Length));
+            return Math.Max(first, second);
+        }
+        #endregion
+
+        #region 比较两数组并移除重复项
+        //给定两个int型数组，从integerList中去除掉在valuesList中的值并返回剩余数组
+        public static int[] Remove(int[] integerList, int[] valuesList)
+        {
+            return integerList.Where(x => !valuesList.Contains(x)).ToArray();
+        }
+        #endregion
     }
 }
